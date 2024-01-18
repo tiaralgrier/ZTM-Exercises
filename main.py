3e96dd64ee9d9e76b34320dd68f808050b0a7ceb
tiaralgrier <80118681+tiaralgrier@users.noreply.github.com>
2024-01-18 22:31:37 +0000

    Exercise 2 push

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+# Exercise 2: Password Checker
+username = input('Enter your username: ') # Ask for username
+password = input('Enter your password: ') # Ask for password
+
+password_length = len(password)
+password_stars = '*' * password_length
+
+# Read password and length to username
+print(f'{username}, your password {password_stars} is {password_length} letters long.')
+
+'''
 # Exercise 1: Type Conversion
 year = input('What year were you born? ')
 age = 2023 - int(year) #Convert year => int
 
 print(f'You are {age} years old!')
+'''
